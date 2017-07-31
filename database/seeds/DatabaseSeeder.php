d4067bc3e6025ad0997a1883c748da4fbfccecce
A Jigoulina <anna.jigoulina@gmail.com>
2017-08-01 00:29:33 +0100

    updated dummy data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -34,10 +34,12 @@ class DatabaseSeeder extends Seeder
 
         DB::table('dojos')->insert([
             [
-                'name' => 'CMW',
-                'address' => 'asdasd',
+                'name' => 'Cocksmoors Woods',
                 'url' => 'http://www.cocksmoorsaikido.co.uk/',
                 'info' => json_encode([
+                    'address' => 'Cocksmoor Woods Leisure Centre, Alcester Rd South, Birmingham, West Midlands B14 6ER',
+                    'shortDescription' => 'Short description for CMW... mus tristique eleifend lorem, nec pulvinar lacus lobortis non. Duis id justo tellus. Donec ultrices enim sit amet libero tempus, vel fringilla urna fringilla. Nulla a quam',
+                    'classes' => 'Wed, Thur',
                     'info' => 'test'
                 ]),
                 'teacher_id' => 2,
@@ -45,10 +47,12 @@ class DatabaseSeeder extends Seeder
                 'longitude' => -1.8970728
             ],
             [
-                'name' => 'todor grange',
-                'address' => 'asdasd',
+                'name' => 'Tudor Grange',
                 'url' => 'http://www.tudorgrangeaikido.co.uk/Pages/default.aspx',
                 'info' => json_encode([
+                    'address' => 'Tudor Grange Sports Centre, Tudor Grange Park, Blossomfield Road, Solihull, Birmingham B91 1NB',
+                    'shortDescription' => 'Short description for Tudor Grange... Sed interdum fringilla iaculis. Suspendisse commodo eros id risus iaculis, sed fermentum purus consequat. Duis sit amet elementum nunc. Pellentesque sed mi dui. Morbi tincidunt accumsan augue, n',
+                    'classes' => 'Mon',
                     'info' => 'test'
                 ]),
                 'teacher_id' => 3,
