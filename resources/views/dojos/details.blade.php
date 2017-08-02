534df241e6acfef1abbb9c9f22378006c05fcb71
A Jigoulina <anna.jigoulina@gmail.com>
2017-08-02 23:17:22 +0100

    putting in some placeholders on the dojo page

--- resources/views/dojos/details.blade.php
+++ resources/views/dojos/details.blade.php
@@ -29,38 +29,49 @@
                     <div id="dojo-map" class="same-height"></div>
                 </div>
 
-                <div class="col-sm-4">
-
-                    <div class="thumbnail same-height">
-                        <div class="caption">
-                            <h3>Timetable</h3>
-                        </div>
-
-                        @foreach($dojo->info['timetable'] as $timetable)
-                            <h4> {{ $timetable['day'] }} </h4>
-
-                            <table class="table table-hover table-bordered">
-                                <tbody>
-                                    <tr>
-                                        <th>Time</th>
-                                        <th>Age</th>
-                                        <th>Class</th>
-                                    </tr>
-
-                                    @foreach($timetable['times'] as $times)
-                                        <tr>
-                                            <td> {{ $times['time'] }} </td>
-                                            <td> {{ $times['age'] }} </td>
-                                            <td> {{ $times['class'] }} </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        @endforeach
+                <div class="col-sm-4 same-height">
+                    <div class="caption">
+                        <h3>Timetable</h3>
                     </div>
 
+                    @foreach($dojo->info['timetable'] as $timetable)
+                        <h4> {{ $timetable['day'] }} </h4>
+
+                        <table class="table table-hover table-bordered">
+                            <tbody>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Age</th>
+                                    <th>Class</th>
+                                </tr>
+
+                                @foreach($timetable['times'] as $times)
+                                    <tr>
+                                        <td> {{ $times['time'] }} </td>
+                                        <td> {{ $times['age'] }} </td>
+                                        <td> {{ $times['class'] }} </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endforeach
                 </div>
-            
+            </div>
+
+            <div class="panel panel-danger">
+                <div class="panel-heading">Some kind of notice!!</div>
+                <div class="panel-body">
+                    Classes are cancelled on days: x, y, z...
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Extra information</h3>
+                    <p>
+                        Aenean sollicitudin consectetur laoreet. Aenean posuere, mauris vitae accumsan mattis, elit libero bibendum neque, vitae elementum purus risus in ante. Aenean quis ex eget augue tristique venenatis. Vivamus non consectetur felis, ornare pharetra erat. In et facilisis enim. Fusce libero leo, pharetra sed lacus in, euismod aliquet metus. Aenean faucibus accumsan vestibulum. Suspendisse potenti. Proin convallis condimentum risus, id mattis neque pellentesque rutrum. Donec pulvinar quis sem non facilisis.
+                    </p>
+                <div>
             </div>
 
         </div>
